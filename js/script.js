f3151a65361dc8dbaeb92df2e27d13713da132fb
Chinonye Ekpendu <alexekpendu001@gmail.com>
2024-10-15 05:50:02 -0800

    navbar mediaqueries + header bg change onScroll

---
+++ js/script.js
@@ -0,0 +1,21 @@
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menu.addEventListener("click", function () {
+    navbar.classList.toggle("active");
+});
+
+window.onscroll = () => {
+    navbar.classList.remove("active");
+}
+
+// Select the header
+const header = document.getElementById('header');
+// Add an event listener to change navbar style on scroll
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
